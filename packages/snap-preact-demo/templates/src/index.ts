ea300532ca23464664208555108eb42bbeb2098e
Chris <chris.frazier@searchspring.com>
2026-04-02 09:39:32 -0700

    refactor(config): just moving the templates config back into index since its no longer used in hybrid

--- packages/snap-preact-demo/templates/src/index.ts
+++ packages/snap-preact-demo/templates/src/index.ts
@@ -1,12 +1,123 @@
 import { SnapTemplates, SnapTemplatesConfig } from '@athoscommerce/snap-preact';
 import deepmerge from 'deepmerge';
 import { combineMerge } from '../../snap/src/middleware/functions';
-import { templatesConfig } from './config';
+import { globalStyles } from './styles';
 
-let _templatesConfig: SnapTemplatesConfig = templatesConfig;
+const siteId = 'atkzs2';
+
+// const siteId = '8uyt2m';
+
+// const clientConfig = {
+// 	meta: {
+// 		origin: `https://${siteId}.a.searchspring.io`,
+// 	},
+// 	search: {
+// 		origin: `https://${siteId}.a.searchspring.io`,
+// 	},
+// 	recommend: {
+// 		origin: `https://${siteId}.a.searchspring.io`,
+// 	},
+// 	suggest: {
+// 		origin: `https://${siteId}.a.searchspring.io`,
+// 	},
+// };
+
+export let templatesConfig: SnapTemplatesConfig = {
+	unlocked: false,
+	config: {
+		siteId: siteId,
+		language: 'en',
+		currency: 'usd',
+		platform: 'other',
+		// client: clientConfig
+	},
+
+	plugins: {
+		common: {
+			addToCart: {
+				function: (data) => console.log('added to cart!', data),
+			},
+		},
+	},
+	components: {
+		result: {
+			CustomResult: async () => (await import('./components/Result')).CustomResult,
+		},
+	},
+	theme: {
+		extends: 'pike',
+		variables: {
+			// breakpoints: {
+			// 	mobile: 767,
+			// 	tablet: 1024,
+			// 	desktop: 1280,
+			// },
+			// colors: {
+			// 	primary: '#6d7175',
+			// 	secondary: '#202223',
+			// 	accent: '#333333',
+			// },
+		},
+		style: globalStyles,
+		overrides: {
+			default: {},
+		},
+	},
+	recommendation: {
+		email: {
+			Email: {
+				component: 'RecommendationEmail',
+			},
+		},
+		default: {
+			Default: {
+				component: 'Recommendation',
+			},
+		},
+		bundle: {
+			Bundle: {
+				component: 'RecommendationBundle',
+			},
+		},
+	},
+	search: {
+		targets: [
+			{
+				selector: '#athos-layout',
+				component: 'SearchCollapsible',
+			},
+		],
+		settings: {
+			variants: {
+				showDisabledSelectionValues: true,
+			},
+			// infinite: {
+			// 	backfill: 5,
+			// },
+		},
+	},
+	autocomplete: {
+		targets: [
+			{
+				selector: 'input.athos-ac',
+				component: 'AutocompleteFixed',
+			},
+		],
+		settings: {
+			history: {
+				limit: 6,
+				showResults: true,
+			},
+			trending: {
+				limit: 6,
+				showResults: true,
+			},
+		},
+	},
+};
 
 if (window.mergeSnapConfig) {
-	_templatesConfig = deepmerge(templatesConfig as object, window.mergeSnapConfig, { arrayMerge: combineMerge }) as SnapTemplatesConfig;
+	templatesConfig = deepmerge(templatesConfig as object, window.mergeSnapConfig, { arrayMerge: combineMerge }) as SnapTemplatesConfig;
 }
 
-new SnapTemplates(_templatesConfig);
+new SnapTemplates(templatesConfig);
